7d297ace16ec033ef7c680f20eabdf3498f8fbd4
Mykola Kovalenko <mykola.kovalenko@snowflake.com>
2025-03-06 09:09:48 +0100

    Fixes

--- dbs/showlog.sql
+++ dbs/showlog.sql
@@ -1,26 +1,30 @@
-SET DBS = '<% dbsname %>';
-SET XMA = '<% depname %>';
-SET SVC = '<% depname %>_SVC';
-SET USR = '<% depname %>_USR';
-SET ROL = '<% depname %>_ROL';
-SET CPL = '<% depname %>_CPL';
-SET VWH = '<% depname %>_VWH';
-SET NRL = '<% depname %>_NRL';
-SET EAI = '<% depname %>_EAI';
-SET DEP = 'dep-<% depname %>';
+SET TARGET_DATABASE   = '<% dbsname %>';
+SET DEPLOY_SCHEMA     = '<% depname %>';
+SET DEPLOY_ROLE_OWNER = '<% depname %>_ROL';
+SET SERVICE_NAME      = '<% depname %>_SVC';
+SET SERVICE_RUN_USER  = '<% depname %>_USR';
+SET SERVICE_RUN_POOL  = '<% depname %>_CPL';
+SET SERVICE_RUN_VWHS  = '<% depname %>_VWH';
+SET EXT_ACC_INT_NAME  = '<% depname %>_EAI';
+SET EXT_ACC_NET_RULE  = '<% depname %>_NRL';
+SET SERVICE_REF       = 'dep-<% depname %>';
 
 ---------------------------------------------------------------------------------
-USE ROLE IDENTIFIER($ROL);
-USE DATABASE IDENTIFIER($DBS);
-USE SCHEMA IDENTIFIER($XMA);
-USE WAREHOUSE IDENTIFIER($VWH);
+USE ROLE IDENTIFIER($DEPLOY_ROLE_OWNER);
+
+USE DATABASE IDENTIFIER($TARGET_DATABASE);
+USE SCHEMA IDENTIFIER($DEPLOY_SCHEMA);
+USE WAREHOUSE IDENTIFIER($SERVICE_RUN_VWHS);
 
 SELECT $1 from @SPECS/service.yaml; 
 
 SHOW SERVICES;
 
 -- check the status of service
-CALL SYSTEM$GET_SERVICE_STATUS($SVC);
+CALL SYSTEM$GET_SERVICE_STATUS($SERVICE_NAME);
 
 -- check the logs in the docker containers
-CALL SYSTEM$GET_SERVICE_LOGS($SVC, '0', $DEP, 1000);
+CALL SYSTEM$GET_SERVICE_LOGS($SERVICE_NAME, '0', $SERVICE_REF, 1000);
+
+SHOW SERVICE CONTAINERS IN SERVICE IDENTIFIER($SERVICE_NAME);
+SHOW ENDPOINTS IN SERVICE IDENTIFIER($SERVICE_NAME);
